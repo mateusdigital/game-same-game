db0abb84135e1e6dfdd8dbe79cd6e8b3835f3268
stdmatt <stdmatt@pixelwizards.io>
2020-05-09 22:15:29 +0300

    Move the flood fill find to the library

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,8 +1,14 @@
 
-
+//----------------------------------------------------------------------------//
+// Constants                                                                  //
+//----------------------------------------------------------------------------//
 const GROUND_HEIGHT   = 130;
 const GAME_HUD_HEIGHT = 0;
 
+//----------------------------------------------------------------------------//
+// Types                                                                      //
+//----------------------------------------------------------------------------//
+//------------------------------------------------------------------------------
 class FixedSizeContainer
     extends PIXI.Container
 {
@@ -11,6 +17,7 @@ class FixedSizeContainer
     }
 }
 
+//------------------------------------------------------------------------------
 class GameScene
     extends Base_Scene
 {
@@ -378,87 +385,4 @@ class GameScene
 
 } // class GameScene
 
-function Algo_FloodFind(container, start_x, start_y,  predicate)
-{
-    const XY     = (x, y)  => { return {x:x, y:y}     };
-    const COPY   = (xy)    => { return XY(xy.x, xy.y) };
-    const GET    = (c, xy) => { return c[xy.y][xy.x]  };
-    const VISITED = (c, xy) => { return Array_Contains(c, (_xy)=>{
-        let b = _xy.x == xy.x && _xy.y == xy.y
-        return b;
-    })};
 
-    let result  = [];
-    let queue   = [XY(start_x, start_y)];
-    let visited = [];
-
-    while(queue.length != 0) {
-        let coord    = Array_PopFront(queue);
-        let matching = [];
-
-        if(VISITED(visited, coord)) {
-            continue;
-        }
-        visited.push(coord);
-
-        const item = GET(container, coord);
-        if(!predicate(item)) {
-            continue;
-        }
-        result.push(item);
-
-        // Search left...
-        for(let left_coord = XY(coord.x -1, coord.y);
-            left_coord.x >= 0;
-            --left_coord.x)
-        {
-            const item = GET(container, left_coord);
-            if(!predicate(item)) {
-                break;
-            }
-            matching.push(COPY(left_coord));
-        }
-        // Search right...
-        for(let right_coord = XY(coord.x + 1, coord.y);
-            right_coord.x < container[right_coord.y].length;
-            ++right_coord.x)
-        {
-            const item = GET(container, right_coord);
-            if(!predicate(item)) {
-                break;
-            }
-            matching.push(COPY(right_coord));
-        }
-        // Search top...
-        for(let top_coord = XY(coord.x, coord.y -1);
-            top_coord.y >= 0;
-            --top_coord.y)
-        {
-            const item = GET(container, top_coord);
-            if(!predicate(item)) {
-                break;
-            }
-            matching.push(COPY(top_coord));
-        }
-        // Search bottom...
-        for(let bottom_coord = XY(coord.x, coord.y +1);
-            bottom_coord.y < container.length;
-            ++bottom_coord.y)
-        {
-            const item = GET(container, bottom_coord);
-            if(!predicate(item)) {
-                break;
-            }
-            matching.push(COPY(bottom_coord));
-        }
-
-        for(let i = 0; i < matching.length; ++i) {
-            const match_coord = matching[i]
-            const match_item  = GET(container, match_coord);
-
-            queue .push(match_coord);
-        }
-    }
-
-    return result;
-}
