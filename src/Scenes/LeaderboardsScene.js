495aaee3e6b4f3ffd4212a679b9d4b32e6f86807
stdmatt <stdmatt@pixelwizards.io>
2020-05-25 11:10:29 +0300

    Start to implement the scene correctly

--- src/Scenes/LeaderboardsScene.js
+++ src/Scenes/LeaderboardsScene.js
@@ -1,23 +1,102 @@
-
-
+//----------------------------------------------------------------------------//
+// Constants                                                                  //
+//----------------------------------------------------------------------------//
 const LEADERBOARD_SCENE_MODE_VIEW  = 0;
 const LEADERBOARD_SCENE_MODE_ENTER = 1;
 
+//----------------------------------------------------------------------------//
+// Types                                                                      //
+//----------------------------------------------------------------------------//
 class LeaderboardsScene
     extends AnimatedScene
 {
+
+    //--------------------------------------------------------------------------
     constructor(scene_mode)
     {
         super();
+
+        //
+        // Housekeeping
+        this.scene_mode        = scene_mode;
+        this._leaderboard_data = null;
+
         //
         // Scenario
         this.sky      = new SkyBackground();
         this.scenario = new ScenarioLayer();
+
         this.addChild(this.sky);
         this.addChild(this.scenario);
 
         //
-        // Buttons
+        // UI
+        this.title_text             = null;
+        this.back_button            = null;
+        this.data_fetch_status_text = null;
+        this.leaderboard_entries    = null;
+
+        this._CreateStaticUI();
+
+        //
+        // Animations
+        this.data_fetch_bubble_tween = null;
+    }
+
+    //--------------------------------------------------------------------------
+    _OnLeaderboardFetched()
+    {
+        //
+        // Stop the Fetching Animation.
+        this.data_fetch_bubble_tween.stop();
+        this.data_fetch_bubble_tween = null;
+
+        Tween_Scale(this.data_fetch_status_text, 500, 0)
+            .onComplete(()=>{
+                RemoveFromParent(this.data_fetch_status_text);
+            })
+            .start();
+
+        // Start the Entries animation
+        this._leaderboard_data = LeaderboardsUtils_GetData();
+        this._CreateLeaderboardUI();
+    } // _OnLeaderboardFetched
+
+    //--------------------------------------------------------------------------
+    _OnLeaderboardFetchError()
+    {
+       this.data_fetch_bubble_tween.stop();
+       this.data_fetch_bubble_tween = null;
+
+       Tween_Scale(this.data_fetch_status_text, 500, 1).start();
+       this.data_fetch_status_text.text = "Sorry, failed to fetch...";
+    } // _OnLeaderboardFetchError
+
+    //--------------------------------------------------------------------------
+    OnFinishedEnterAnimation()
+    {
+        this.data_fetch_bubble_tween = Tween_CreateBasic(1000)
+            .onUpdate((v)=>{
+                const scale = Math_Map(v.value, 0, 1, 0.9, 1.1);
+                this.data_fetch_status_text.scale.set(scale);
+            })
+            .yoyo(true)
+            .repeat(Infinity)
+            .easing(TWEEN.Easing.Sinusoidal.InOut)
+            .start();
+
+
+        LeaderboardsUtils_FetchFromServer(
+            ()=>{ this._OnLeaderboardFetched   () },
+            ()=>{ this._OnLeaderboardFetchError() }
+        );
+    } // _OnFinishedEnterAnimation
+
+    //--------------------------------------------------------------------------
+    _CreateStaticUI()
+    {
+        //
+        // Back Button.
         this.back_button = new NineSliceButton(
             ORANGE_TEXTURE_SETTINGS,
             NINE_SLICE_SETTINGS,
@@ -32,79 +111,105 @@ class LeaderboardsScene
         this.back_button.AddIcon(Sprite_Create(BUTTON_ICON_NAME_BACK));
         this.addChild(this.back_button);
 
+        //
+        // Title.
+        this.title_text = new Base_BMPText(
+            "LEADERBOARDS",
+            BIG_FONT_NAME,
+            BIG_FONT_SIZE
+        );
+        Center_Anchor(this.title_text);
 
-        this.scene_mode = scene_mode;
-        this._leaderboard_data = LeaderboardsUtils_GetData();
+        this.title_text.x = GAME_DESIGN_WIDTH  * 0.5;
+        this.title_text.y = GAME_DESIGN_HEIGHT * 0.1;
+        this.addChild(this.title_text);
 
-        let curr_y = 150;
-        const font_family = "ARCO Typography";
-        const LEADERBOARDS_TITLE_FONT_SIZE = 82;
-        const title_text =  new Base_BMPText("LEADERBOARDS", font_family, LEADERBOARDS_TITLE_FONT_SIZE);
-        Update_Anchor(title_text, 0.5);
-        title_text.x = GAME_DESIGN_WIDTH  * 0.5;
-        title_text.y = GAME_DESIGN_HEIGHT * 0.1;
-        this.addChild(title_text);
+        //
+        // Data Fetch Status.
+        this.data_fetch_status_text = new Base_BMPText(
+            "Fetching leaderboards...",
+            MEDIUM_FONT_NAME,
+            MEDIUM_FONT_SIZE
+        );
+        Center_Anchor(this.data_fetch_status_text);
+        Debug_Tint(this.data_fetch_status_text);
 
-        const max_scale = 1.1;
-        const min_scale = 0.7;
-        const data_len  = this._leaderboard_data.length;
-        const entry_font_size = 46;
+        this.data_fetch_status_text.x = GAME_DESIGN_WIDTH  * 0.5;
+        this.data_fetch_status_text.y = GAME_DESIGN_HEIGHT * 0.5;
+        this.data_fetch_status_text.scale.set(0);
+        this.addChild(this.data_fetch_status_text);
+    } // _CreateStaticUI
 
+    //--------------------------------------------------------------------------
+    _CreateLeaderboardUI()
+    {
+        const MAX_CHARS_NAME     = 10;
+        const MAX_ENTRIES_COUNT  = 10;
+        const POSITION_DIGITS    = 2;
+        const ENTRY_FONT_NAME    = BIG_FONT_NAME;
+        const POSITION_FONT_SIZE = 30;
+        const NAME_FONT_SIZE     = 41;
+        const SCORE_FONT_SIZE    = 32;
 
-        for(let i = 0; i < data_len; ++i) {
-            const entry = this._leaderboard_data[i];
+        const ENTRY_MAX_SCALE = 1.0;
+        const ENTRY_MIN_SCALE = 0.8;
 
-
-            let name_str = entry.name;
-            if(name_str.length > 10) {
-                name_str = name_str.substring(0, 10)
-            }
-
-            const pos_str = String_Cat("#", FillDigits(i+1, 2));
+        let curr_y = this.title_text.y + (this.title_text.height * 0.5);
+        const entries_count = Math_Min(
+            MAX_ENTRIES_COUNT,
+            this._leaderboard_data.length
+        );
+        for(let i = 0; i < entries_count; ++i) {
+            // Get the data.
+            const entry     = this._leaderboard_data[i];
+            const name_str  = entry.name.substr(0, MAX_CHARS_NAME);
+            const pos_str   = String_Cat("#", FillDigits(i+1, POSITION_DIGITS));
             const score_str = FillDigits(entry.score);
 
+            // Build the BMP Texts.
+            const curr_scale = Math_Map(
+                i,
+                0, MAX_ENTRIES_COUNT,
+                ENTRY_MAX_SCALE, ENTRY_MIN_SCALE
+            );
 
-            const curr_scale     = Math_Map(i, 0, data_len, max_scale, min_scale);
-            const curr_font_size = entry_font_size * curr_scale;
+            const pos_text   = new Base_BMPText(pos_str,   ENTRY_FONT_NAME, POSITION_FONT_SIZE * curr_scale);
+            const name_text  = new Base_BMPText(name_str,  ENTRY_FONT_NAME, NAME_FONT_SIZE     * curr_scale);
+            const score_text = new Base_BMPText(score_str, ENTRY_FONT_NAME, SCORE_FONT_SIZE    * curr_scale);
+            Debug_Tint(pos_text, name_text, score_text);
+            this.addChild(pos_text);
+            this.addChild(name_text);
+            this.addChild(score_text);
 
-
-            const pos_text   = new Base_BMPText(pos_str,   font_family, 30 * curr_scale);
-            const name_text  = new Base_BMPText(name_str,  font_family, curr_font_size);
-            const score_text = new Base_BMPText(score_str, font_family, 40 * curr_scale);
-
-            score_text.alpha = 0;
-            name_text .alpha = 0;
-            pos_text  .alpha = 0;
-
-            Tween_CreateBasic(300).onUpdate((v)=>{
-                score_text.alpha = v.value;
-                name_text .alpha = v.value;
-                pos_text  .alpha = v.value;
-            }).delay(100 * i).start()
-
-            // Update_Anchor(pos_text,  0.0, 0.5);
-            // Update_Anchor(name_text,  0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000, 0.5);
-            // Update_Anchor(score_text, 0.5, 0.5);
+            // Setup the positioning.
             curr_y += name_text.height + 20;
 
             Update_Anchor(pos_text,   0, 0.5);
             Update_Anchor(score_text, 1, 0.5);
-            Update_Anchor(name_text,   0.0, 0.5);
-            pos_text.x = title_text.x - (title_text.width * 0.5) + 5
+            Update_Anchor(name_text,  0, 0.5);
+
+            pos_text.x = this.title_text.x - (this.title_text.width * 0.5);
             pos_text.y = curr_y + 3 * curr_scale; //TWEaK
 
-
-            score_text.x = title_text.x + (title_text.width * 0.5) -5;
+            score_text.x = this.title_text.x + (this.title_text.width * 0.5);
             score_text.y = curr_y;
-            const a = (pos_text.x + pos_text.width + 10)
-            const b = (score_text.x - score_text.width * 0.5);
-            name_text.x = 88;
+
+            name_text.x = 88; // XXX(stdmatt): Remove magic...
             name_text.y = curr_y;
 
+            // Setup the Animation.
+            score_text.alpha = 0;
+            name_text .alpha = 0;
+            pos_text  .alpha = 0;
 
-            this.addChild(pos_text);
-            this.addChild(name_text);
-            this.addChild(score_text);
+            Tween_CreateBasic(300)
+                .onUpdate((v)=>{
+                    score_text.alpha = v.value;
+                    name_text .alpha = v.value;
+                    pos_text  .alpha = v.value;
+                })
+                .delay(100 * i)
+                .start();
         }
-    }
-}
+    } // _CreateLeaderboardUI
+} // class LeaderboardsScene
