bb7996984a3847ccda3221091d7c93b5b6fe12fc
stdmatt <stdmatt@pixelwizards.io>
2020-05-25 11:10:42 +0300

    Improve the service a little

--- server/same_game_leaderboards/app.py
+++ server/same_game_leaderboards/app.py
@@ -1,76 +1,87 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_cors import CORS
+import werkzeug.exceptions;
 
 app = Flask(__name__)
 CORS(app)
 
-
 POSTGRES = {
-    'user': 'postgres',
-    'pw':   'docker',
+    'user': 'stdmatt',
+    'pw':   '',
     'db':   'same_game',
     'host': 'localhost',
     'port': '5432',
 }
-conn_str = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+conn_str = 'postgresql://%(user)s%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+print(conn_str);
 
 app.config['DEBUG'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = conn_str
 
 db = SQLAlchemy()
-db.init_app(app)
-migrate = Migrate(app, db)
-
+db.init_app(app);
 
 class Leaderboard(db.Model):
     __tablename__ = "leaderboard";
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
-    score = db.Column(db.Integer, nullable=False)
+    id    = db.Column(db.Integer,    primary_key=True)
+    name  = db.Column(db.String(20), nullable=False)
+    score = db.Column(db.Integer,    nullable=False)
 
 
+def _get_leaderboards():
+    ## @TODO(stdmatt): Add limits...
+    entries = Leaderboard.query.order_by(Leaderboard.score.desc()).all()
+    results = [{
+        "name":  entry.name,
+        "score": entry.score,
+    } for entry in entries];
 
-_leaderboard_data = [
-    {"name": "mateus", "score": 10}
+    return results;
 
-]
-def get_leaderboards():
-    return _leaderboard_data
 
-def insert_leaderboard_entry(data):
-    name = data.get("name", "");
-    score = data.get("score", "");
+def _insert_leaderboard_entry(data):
+    name  = data.get("name",  None);
+    score = data.get("score", None);
 
-    _leaderboard_data.append({
-        "name": name,
-        "score": int(score)
-    })
+    if(name is None or len(name) == 0):
+        return False;
+    if(score is None or not isinstance(score, int)):
+        return False;
+
+    clean_data = {
+        "name":  name,
+        "score": score
+    }
+
+    entry = Leaderboard(**data)
+    db.session.add(entry)
+    db.session.commit()
+
+    return True;
 
 
 @app.route("/leaderboards", methods=["GET", "POST"])
 def hello():
+    db.create_all();
     if request.method == "GET":
-        entries = Leaderboard.query.order_by(Leaderboard.score.desc()).all()
-        results = [{
-            "name":  entry.name,
-            "score": entry.score,
-        } for entry in entries]
-        return jsonify(results)
+        results = _get_leaderboards();
+        return jsonify(results);
+
     elif request.method == "POST":
-        data = request.get_json(force=True);
+        result = { "success": False };
+
         try:
-            entry = Leaderboard(**data)
-            db.session.add(entry)
-            db.session.commit()
-
-            return jsonify({"success": True})
+            data = request.get_json(force=True);
+            result["success"] = _insert_leaderboard_entry(data);
+        except werkzeug.exceptions.BadRequest as err:
+            ## @TODO(stdmatt): Log...
+            result["error_msg"] = "Missing request body.";
         except Exception as err:
-            return jsonify({"success": False})
-
-
-
+            print(str(err));
+            raise err;
+        finally:
+            return jsonify(result);
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
