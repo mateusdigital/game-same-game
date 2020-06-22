e190121cfba04ffc35b83585ba90a2110a44183c
stdmatt <stdmatt@pixelwizards.io>
2020-06-23 02:27:20 +0300

    Update the texture list

--- src/Textures.js
+++ src/Textures.js
@@ -1,115 +1,63 @@
-const TEST = "res/textures/test.png"
-const LEADERBOARDS = "res/textures/Leaderboards.png"
-const LOGO = "res/textures/LOGO.png"
-const BRICKS_4 = "res/textures/bricks/4.png"
-const BRICKS_5 = "res/textures/bricks/5.png"
-const BRICKS_2 = "res/textures/bricks/2.png"
-const BRICKS_3 = "res/textures/bricks/3.png"
-const BRICKS_1 = "res/textures/bricks/1.png"
-const BACKGROUND_CLOUD_1 = "res/textures/background/cloud_1.png"
-const BACKGROUND_CLOUD_0 = "res/textures/background/cloud_0.png"
-const BACKGROUND_CLOUD_2 = "res/textures/background/cloud_2.png"
-const NUMBERS_8 = "res/textures/numbers/8.png"
-const NUMBERS_9 = "res/textures/numbers/9.png"
-const NUMBERS_4 = "res/textures/numbers/4.png"
-const NUMBERS_5 = "res/textures/numbers/5.png"
-const NUMBERS_7 = "res/textures/numbers/7.png"
-const NUMBERS_6 = "res/textures/numbers/6.png"
-const NUMBERS_2 = "res/textures/numbers/2.png"
-const NUMBERS_3 = "res/textures/numbers/3.png"
-const NUMBERS_1 = "res/textures/numbers/1.png"
-const NUMBERS_0 = "res/textures/numbers/0.png"
-const BUTTONS_BOX_GREEN_PRESSED = "res/textures/buttons/box/green_pressed.png"
-const BUTTONS_BOX_YELLOW_PRESSED = "res/textures/buttons/box/yellow_pressed.png"
-const BUTTONS_BOX_YELLOW = "res/textures/buttons/box/yellow.png"
-const BUTTONS_BOX_BLUE = "res/textures/buttons/box/blue.png"
-const BUTTONS_BOX_RED_PRESSED = "res/textures/buttons/box/red_pressed.png"
-const BUTTONS_BOX_GREEN = "res/textures/buttons/box/green.png"
-const BUTTONS_BOX_GREY = "res/textures/buttons/box/grey.png"
-const BUTTONS_BOX_BLUE_PRESSED = "res/textures/buttons/box/blue_pressed.png"
-const BUTTONS_BOX_GREY_PRESSED = "res/textures/buttons/box/grey_pressed.png"
-const BUTTONS_BOX_RED = "res/textures/buttons/box/red.png"
-const BUTTONS_ICON_PLUS = "res/textures/buttons/icon/plus.png"
-const BUTTONS_ICON_ARROW_LEFT = "res/textures/buttons/icon/arrow_left.png"
-const BUTTONS_ICON_MUSIC_ON = "res/textures/buttons/icon/music_on.png"
-const BUTTONS_ICON_MUSIC_OFF = "res/textures/buttons/icon/music_off.png"
-const BUTTONS_ICON_LEADERBOARDS = "res/textures/buttons/icon/leaderboards.png"
-const BUTTONS_ICON_CREDITS = "res/textures/buttons/icon/credits.png"
-const BUTTONS_ICON_PLAY = "res/textures/buttons/icon/play.png"
-const FADE_SHAPE_5 = "res/textures/fade/Shape_5.png"
-const PARTICLES_EMOTE_STARS = "res/textures/particles/emote_stars.png"
-const PARTICLES_EMOTE_STAR = "res/textures/particles/emote_star.png"
-const PARTICLES_EMOTE_HEARTS = "res/textures/particles/emote_hearts.png"
-const PARTICLES_EMOTE_HEART = "res/textures/particles/emote_heart.png"
-const CREDITS_MSG_9 = "res/textures/credits/msg_9.png"
-const CREDITS_MSG_8 = "res/textures/credits/msg_8.png"
-const CREDITS_MSG_3 = "res/textures/credits/msg_3.png"
-const CREDITS_MSG_2 = "res/textures/credits/msg_2.png"
-const CREDITS_MSG_1 = "res/textures/credits/msg_1.png"
-const CREDITS_MSG_14 = "res/textures/credits/msg_14.png"
-const CREDITS_MSG_10 = "res/textures/credits/msg_10.png"
-const CREDITS_MSG_5 = "res/textures/credits/msg_5.png"
-const CREDITS_MSG_4 = "res/textures/credits/msg_4.png"
-const CREDITS_MSG_11 = "res/textures/credits/msg_11.png"
-const CREDITS_MSG_13 = "res/textures/credits/msg_13.png"
-const CREDITS_MSG_6 = "res/textures/credits/msg_6.png"
-const CREDITS_MSG_7 = "res/textures/credits/msg_7.png"
-const CREDITS_MSG_12 = "res/textures/credits/msg_12.png"
+const RES_TEXTURES_LEADERBOARDS = "res/textures/Leaderboards.png"
+const RES_TEXTURES_LOGO = "res/textures/LOGO.png"
+const RES_TEXTURES_TEST = "res/textures/test.png"
+const RES_TEXTURES_BACKGROUND_CLOUD_0 = "res/textures/background/cloud_0.png"
+const RES_TEXTURES_BACKGROUND_CLOUD_1 = "res/textures/background/cloud_1.png"
+const RES_TEXTURES_BACKGROUND_CLOUD_2 = "res/textures/background/cloud_2.png"
+const RES_TEXTURES_BRICKS_1 = "res/textures/bricks/1.png"
+const RES_TEXTURES_BRICKS_2 = "res/textures/bricks/2.png"
+const RES_TEXTURES_BRICKS_3 = "res/textures/bricks/3.png"
+const RES_TEXTURES_BRICKS_4 = "res/textures/bricks/4.png"
+const RES_TEXTURES_BRICKS_5 = "res/textures/bricks/5.png"
+const RES_TEXTURES_BUTTONS_BOX_BLUE = "res/textures/buttons/box/blue.png"
+const RES_TEXTURES_BUTTONS_BOX_BLUE_PRESSED = "res/textures/buttons/box/blue_pressed.png"
+const RES_TEXTURES_BUTTONS_BOX_GREEN = "res/textures/buttons/box/green.png"
+const RES_TEXTURES_BUTTONS_BOX_GREEN_PRESSED = "res/textures/buttons/box/green_pressed.png"
+const RES_TEXTURES_BUTTONS_BOX_RED = "res/textures/buttons/box/red.png"
+const RES_TEXTURES_BUTTONS_BOX_RED_PRESSED = "res/textures/buttons/box/red_pressed.png"
+const RES_TEXTURES_BUTTONS_BOX_YELLOW = "res/textures/buttons/box/yellow.png"
+const RES_TEXTURES_BUTTONS_BOX_YELLOW_PRESSED = "res/textures/buttons/box/yellow_pressed.png"
+const RES_TEXTURES_BUTTONS_ICON_ARROW_LEFT = "res/textures/buttons/icon/arrow_left.png"
+const RES_TEXTURES_BUTTONS_ICON_CREDITS = "res/textures/buttons/icon/credits.png"
+const RES_TEXTURES_BUTTONS_ICON_LEADERBOARDS = "res/textures/buttons/icon/leaderboards.png"
+const RES_TEXTURES_BUTTONS_ICON_MUSIC_OFF = "res/textures/buttons/icon/music_off.png"
+const RES_TEXTURES_BUTTONS_ICON_MUSIC_ON = "res/textures/buttons/icon/music_on.png"
+const RES_TEXTURES_BUTTONS_ICON_PLAY = "res/textures/buttons/icon/play.png"
+const RES_TEXTURES_BUTTONS_ICON_PLUS = "res/textures/buttons/icon/plus.png"
+const RES_TEXTURES_FADE_SHAPE_5 = "res/textures/fade/Shape_5.png"
+const RES_TEXTURES_PARTICLES_EMOTE_HEART = "res/textures/particles/emote_heart.png"
+const RES_TEXTURES_PARTICLES_EMOTE_HEARTS = "res/textures/particles/emote_hearts.png"
+const RES_TEXTURES_PARTICLES_EMOTE_STAR = "res/textures/particles/emote_star.png"
+const RES_TEXTURES_PARTICLES_EMOTE_STARS = "res/textures/particles/emote_stars.png"
 
-const TEXTURES_TO_LOAD = [ "res/textures/test.png",
-"res/textures/Leaderboards.png",
+const TEXTURES_TO_LOAD = [ "res/textures/Leaderboards.png",
 "res/textures/LOGO.png",
-"res/textures/bricks/4.png",
-"res/textures/bricks/5.png",
+"res/textures/test.png",
+"res/textures/background/cloud_0.png",
+"res/textures/background/cloud_1.png",
+"res/textures/background/cloud_2.png",
+"res/textures/bricks/1.png",
 "res/textures/bricks/2.png",
 "res/textures/bricks/3.png",
-"res/textures/bricks/1.png",
-"res/textures/background/cloud_1.png",
-"res/textures/background/cloud_0.png",
-"res/textures/background/cloud_2.png",
-"res/textures/numbers/8.png",
-"res/textures/numbers/9.png",
-"res/textures/numbers/4.png",
-"res/textures/numbers/5.png",
-"res/textures/numbers/7.png",
-"res/textures/numbers/6.png",
-"res/textures/numbers/2.png",
-"res/textures/numbers/3.png",
-"res/textures/numbers/1.png",
-"res/textures/numbers/0.png",
-"res/textures/buttons/box/green_pressed.png",
-"res/textures/buttons/box/yellow_pressed.png",
-"res/textures/buttons/box/yellow.png",
+"res/textures/bricks/4.png",
+"res/textures/bricks/5.png",
 "res/textures/buttons/box/blue.png",
-"res/textures/buttons/box/red_pressed.png",
-"res/textures/buttons/box/green.png",
-"res/textures/buttons/box/grey.png",
 "res/textures/buttons/box/blue_pressed.png",
-"res/textures/buttons/box/grey_pressed.png",
+"res/textures/buttons/box/green.png",
+"res/textures/buttons/box/green_pressed.png",
 "res/textures/buttons/box/red.png",
-"res/textures/buttons/icon/plus.png",
+"res/textures/buttons/box/red_pressed.png",
+"res/textures/buttons/box/yellow.png",
+"res/textures/buttons/box/yellow_pressed.png",
 "res/textures/buttons/icon/arrow_left.png",
-"res/textures/buttons/icon/music_on.png",
-"res/textures/buttons/icon/music_off.png",
-"res/textures/buttons/icon/leaderboards.png",
 "res/textures/buttons/icon/credits.png",
+"res/textures/buttons/icon/leaderboards.png",
+"res/textures/buttons/icon/music_off.png",
+"res/textures/buttons/icon/music_on.png",
 "res/textures/buttons/icon/play.png",
+"res/textures/buttons/icon/plus.png",
 "res/textures/fade/Shape_5.png",
-"res/textures/particles/emote_stars.png",
-"res/textures/particles/emote_star.png",
-"res/textures/particles/emote_hearts.png",
 "res/textures/particles/emote_heart.png",
-"res/textures/credits/msg_9.png",
-"res/textures/credits/msg_8.png",
-"res/textures/credits/msg_3.png",
-"res/textures/credits/msg_2.png",
-"res/textures/credits/msg_1.png",
-"res/textures/credits/msg_14.png",
-"res/textures/credits/msg_10.png",
-"res/textures/credits/msg_5.png",
-"res/textures/credits/msg_4.png",
-"res/textures/credits/msg_11.png",
-"res/textures/credits/msg_13.png",
-"res/textures/credits/msg_6.png",
-"res/textures/credits/msg_7.png",
-"res/textures/credits/msg_12.png" ]
+"res/textures/particles/emote_hearts.png",
+"res/textures/particles/emote_star.png",
+"res/textures/particles/emote_stars.png" ]
